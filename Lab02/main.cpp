9101dd2ebe87a924b433b6dd10e171ef8068bdf1
Владік Путінца <vladputintsa@gmail.com>
2026-03-21 19:02:26 +0200

    Update main.cpp with full 3-level hierarchy tests

--- Lab02/main.cpp
+++ Lab02/main.cpp
@@ -1,20 +1,31 @@
 #include "Brawler.h"
 #include "GameMap.h"
 #include "Gadget.h"
+#include "TankBrawler.h"
+#include "Shield.h"
 #include <iostream>
 
 int main () {
-    std::cout << "Brawler Count: " << Brawler::getBrawlerCount() << std::endl;
+    std::cout << "Initial Brawler Count: " << Brawler::getBrawlerCount() << std::endl;
 
     Gadget invisibility("Invisibility Cloak", 3);
-    Brawler leon("Leon", 2400, 11, invisibility);
     Gadget speeeeeed("Speed Booster", 3);
-    Brawler carl("Carl", 3440, 11, speeeeeed);
     Gadget trainingGadget("Training Gadget", 5);
-    const Brawler trainer ("Training Bot", 10000, 1, trainingGadget);
+
+    Brawler leon("Leon", 2400, 11, invisibility, 0.0, 0.0, 1);
+    Brawler carl("Carl", 3440, 11, speeeeeed, 5.0, 5.0, 2);
+
+    TankBrawler frank("Frank", 14000, 10, trainingGadget, 100, Shield("Iron", 2000), 10.0, 10.0, 3);
+
+    const Brawler trainer ("Training Bot", 10000, 1, trainingGadget, 50.0, 50.0, 999);
+
     GameMap cavernChurn("Cavern Churn", "Showdown", "Desert", "None");
 
-    std::cout << "Brawler Count: " << Brawler::getBrawlerCount() << std::endl;
+    std::cout << "Brawler Count after creation: " << Brawler::getBrawlerCount() << std::endl;
+
+    frank.move(2.0, 3.0);
+    frank.showPosition();
+    frank.takeDamage(50);
 
     Brawler copyOfLeon = leon;
     copyOfLeon = carl;
@@ -26,7 +37,7 @@ int main () {
     }
 
     ++leon;
-    std::cout << leon;      
+    std::cout << leon;     
     carl.showInfo();      
     trainer.showInfo();   
     leon.useAbility();    
